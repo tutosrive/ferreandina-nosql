f307c70d865df34e9aa03817374737508d0cf4ff
tutosrive <tutosrivegamer@gmail.com>
2026-06-21 11:43:09 -0500

    Updates to a functional CRUDs completly, just missing custom queries

--- app/bc/app/src/main/java/com/ferreandina/services/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.ferreandina.services;
-
-import com.ferreandina.models.CustomerModel;
-
-public class CustomerService extends Service<CustomerModel> {
-    public CustomerService() {
-        super(CustomerModel.class);
-    }
-}
--- app/bc/app/src/main/java/com/ferreandina/utils/Utils.java
+++ app/bc/app/src/main/java/com/ferreandina/utils/Utils.java
@@ -1,8 +1,8 @@
 package com.ferreandina.utils;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-
+import java.util.List;
+import java.util.Map;
 import org.bson.conversions.Bson;
 import org.json.JSONObject;
 import com.ferreandina.models.Model;
@@ -20,5 +20,23 @@ public class Utils {
         });
         Bson updatesBson = Updates.combine(updates);
         return updatesBson;
+    }
+
+    public final static Bson fromMapToBsonUpdate(Map<String, Object> map) {
+        ArrayList<Bson> updates = new ArrayList<>();
+        map.forEach((key, value) -> {
+            if (!key.equalsIgnoreCase("id") && !key.equalsIgnoreCase("_id")) {
+                if (value instanceof List) {
+                    @SuppressWarnings("unchecked")
+                    List<Object> listValue = (List<Object>) value;
+                    Bson row = Updates.pushEach(key, listValue);
+                    updates.add(row);
+                } else {
+                    Bson row = Updates.set(key, value);
+                    updates.add(row);
+                }
+            }
+        });
+        return Updates.combine(updates);
     }
 }
